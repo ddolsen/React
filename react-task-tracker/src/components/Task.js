e6ef8b8559ac0a68a98162bcf8049adb76cbd1d9
ddolsen <devin.d.olsen@gmail.com>
2021-10-04 20:52:18 -0500

    Update react-task-tracker project

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -10,7 +10,7 @@ const Task = ({ task, onDelete, onToggle }) => {
         {task.text}{' '}
         <FaTimes
           style={{ color: 'red', cursor: 'pointer' }}
-          onClick={onDelete}
+          onClick={() => onDelete(task.id)}
         />
       </h3>
       <p>{task.day}</p>
